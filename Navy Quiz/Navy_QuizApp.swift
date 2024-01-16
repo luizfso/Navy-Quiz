71f77c8259bd12cd4cf762613700a323142690bc
Luiz Santiago <luiz@mswebmaker.com>
2024-01-16 18:15:10 -0500

    Initial Commit

---
+++ Navy Quiz/Navy_QuizApp.swift
@@ -0,0 +1,32 @@
+//
+//  Navy_QuizApp.swift
+//  Navy Quiz
+//
+//  Created by Luiz Santiago on 1/16/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Navy_QuizApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
